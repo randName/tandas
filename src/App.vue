d09d38215c3a2c30a4df443a94b89950a99c2628
Shun Git <kwokshungit@gmail.com>
2019-01-13 11:09:42 +0800

    basic app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,103 @@
 <template>
 <v-app :dark="dark">
+  <v-toolbar app>
+    <v-toolbar-title class="headline">
+      <span>Tandas</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-chip>
+      <v-avatar>
+        <v-icon v-text="status.icon" :color="status.color"></v-icon>
+      </v-avatar>
+      {{ status.text }}
+    </v-chip>
+  </v-toolbar>
+  <v-content>
+    <v-container fluid grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card>
+            <v-card-title class="headline">Quick Actions</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn disabled>Queue <v-icon right>layers</v-icon></v-btn>
+              <v-btn @click="use" color="primary" :disabled="goDisable">
+                {{ useText }}
+                <v-icon right>directions_run</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </v-app>
 </template>
 
 <script>
+import firestore from '@/plugins/firebase'
+
 export default {
   data () {
     return {
       dark: true,
+      activity: [],
+      requested: false,
     }
   },
+  firestore,
+  computed: {
+    useText () {
+      return this.requested ? 'End' : 'Go'
+    },
+    goDisable () {
+      return this.status.used && !this.requested
+    },
+    recent () {
+      const duration = null
+      return this.activity.map((h) => ({ ...h, ts: h.ts.seconds }))
+        .reduce((o, h) => {
+          if (o[0].start) {
+            if (!h.oc) {
+              o[0].end = h.ts
+              o[0].duration = Math.round((h.ts - o[0].start) / 6, 1) / 10
+              o.unshift({ duration })
+            }
+          } else if (h.oc) {
+            o[0].start = h.ts
+          }
+          return o
+        }, [{ duration }]).filter((l) => l.start).slice(0, 5)
+    },
+    status () {
+      const l = this.recent[0]
+      const used = l ? (!l.end) : null
+      const color = used ? 'error' : 'success'
+      const icon = l ? (used ? 'remove' : 'check') + '_circle' : 'sync'
+      const text = l ? 'Since ' + this.showTime(l.end || l.start) : 'Getting status ...'
+
+      return {
+        used,
+        text,
+        icon,
+        color,
+      }
+    }
+  },
+  methods: {
+    use () {
+      this.$firestoreRefs.door.add({
+        ts: new Date(),
+        oc: !this.status.used
+      }).then(() => {
+        this.requested = !this.requested
+      })
+    },
+    showTime (time) {
+      const ts = new Date(time * 1000)
+      const t = ['Hours', 'Minutes'].map((f) => ts['get' + f]())
+      return t.map((n) => (n > 9 ? '' : '0') + n).join(':')
+    }
+  }
 }
 </script>
---
+++ src/plugins/firebase.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const pid = process.env.VUE_APP_FB_PROJECT
+
+const config = {
+  projectId: pid,
+  storageBucket: `${pid}.appspot.com`,
+  authDomain: `${pid}.firebaseapp.com`,
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  databaseURL: `https://${pid}.firebaseio.com`,
+  messagingSenderId: process.env.VUE_APP_FB_SEND_ID
+}
+
+const d = new Date()
+d.setHours(d.getHours() - 6)
+d.setMinutes(0)
+d.setSeconds(0)
+
+firebase.initializeApp(config)
+const db = firebase.firestore()
+db.settings({ timestampsInSnapshots: true })
+
+const door = db.collection('door')
+const activity = door.where('ts', '>', d).orderBy('ts')
+
+export default function () {
+  return {
+    door,
+    activity,
+  }
+}
